6a012e60e39b573b05459fbbad03e96dd43e4108
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2017-09-25 04:01:38 +0700

    commit25sep17
    ปรับcode รอเพื่อนส่งรายละเอียดมา

--- index.php
+++ index.php
@@ -20,46 +20,36 @@
   <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
 </head>
 
-<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-  
+<body class="fixed-nav sticky-footer bg-dark" id="page-top" style="font-family: 'Kanit', sans-serif;">
+<?php include("components/template/navbar.php") ?>
 
-<div class="content-wrapper" style="font-family: 'Kanit', sans-serif;">
-  <?php include("components/navbar.php") ?>
-  <?php /*nclude("components/countdetail.php")*/ ?>   
+  <div class="content-wrapper" style="font-family: 'Kanit', sans-serif;">
 
 
-    <!-- /.content-wrapper-->
-    <footer class="sticky-footer">
-      <div class="container">
-        <div class="text-center">
-          <small>Copyright © SVOA | KMUTNB </small>
-        </div>
-      </div>
-    </footer>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <?php include("components/scripts.php") ?>
-</div>
+      <?php
+
+
+        $file = (! empty($_GET["depart"])) ? $_GET["depart"] : require ("components/template/countdetail.php"); ;
+        if ($file=="sale")
+            include ("components/sale/index.php");
+        if ($file=="personal")
+            include ("components/personnal/index.php");
+        if ($file=="service")
+            include ("components/personnal/index.php");
+        if ($file=="service")
+            include ("components/personnal/index.php");
+      ?>
+
+      <!-- footer -->
+      <?php include("components/template/footer.php") ?>
+      <!-- footer -->
+
+      <!--logout-->
+      <?php include("components/template/logout.php") ?>
+      <!--logout-->
+      
+      <?php include("components/template/scripts.php") ?>
+  </div>
 </body>
 
 </html>
